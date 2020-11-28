385926108cb559df11c5c8ab443d75697994797c
MilzaOnTwitch <enrico.bono.home@gmail.com>
2020-11-28 16:12:44 +0100

    Initial commit

---
+++ src/main/java/studio/baivo/testenv_sakila/TestenvSakilaApplication.java
@@ -0,0 +1,13 @@
+package studio.baivo.testenv_sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestenvSakilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestenvSakilaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/studio/baivo/testenv_sakila/TestenvSakilaApplicationTests.java
@@ -0,0 +1,13 @@
+package studio.baivo.testenv_sakila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestenvSakilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
