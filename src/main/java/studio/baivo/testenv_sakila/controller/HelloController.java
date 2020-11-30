a50dcabe4ee93ab31cee42b5d765b2ab89a47df8
MilzaOnTwitch <enrico.bono.home@gmail.com>
2020-11-30 02:58:12 +0100

    a

--- src/main/java/studio/baivo/testenv_sakila/controller/HelloController.java
+++ src/main/java/studio/baivo/testenv_sakila/controller/HelloController.java
@@ -1,48 +1,71 @@
 package studio.baivo.testenv_sakila.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 //dynamic request handler
 @Controller
+// globally set up Response body
+@ResponseBody
+// every Request lives under root/trolling/...
+@RequestMapping("trolling")
 public class HelloController {
     //    Handles request at path party
     @GetMapping("party")
-    @ResponseBody
+//    @ResponseBody
     public String party() {
         return "<script>alert('LETTS GOOOO, PARTY 🥳🎉🎉!')</script>";
     }
 
     //    Handles request at path goodbye
     @GetMapping("goodbye")
-    @ResponseBody
+//    @ResponseBody
     public String goobye() {
         return "Hello, Sprint!";
     }
 
-
     //    Handels request of the form /hello?name=LaunchCode
     @GetMapping("helloWithQueryParam")
-    @ResponseBody
+//    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name +"!";
+        return "Hello, " + name + "!";
     }
 
     //    Handels request of the form /hello/LaunchCode
     @GetMapping("Hello/{name}")
-    @ResponseBody
+//    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
+
+    //    Handels both get and post request
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    //   @ResponseBody
+    public String helloRequestMapping(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    //    Handels both get and post request uses Dynamic Templates and The Model
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    //   @ResponseBody
+    public String helloRequestMappingModel(@RequestParam String name, Model model) {
+
+        String greeting = "Hello, " + name + "!";
+
+        model.addAttribute("greeting", greeting);
+        return "Hello, " + name + "!";
+    }
+
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm(){
+//    @ResponseBody
+    public String helloForm() {
 //        returns html bullshit
-        return "<alert>Hello</alert>";
+        return "<script>alert('Hello');</script>";
 
     }
+
+
 
 }
 
