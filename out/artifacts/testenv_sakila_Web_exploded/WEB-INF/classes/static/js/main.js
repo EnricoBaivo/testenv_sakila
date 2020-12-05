8307c000930579634620b6d19195d9cbf422cb8b
MilzaOnTwitch <enrico.bono.home@gmail.com>
2020-12-05 17:08:44 +0100

    brancvh

---
+++ out/artifacts/testenv_sakila_Web_exploded/WEB-INF/classes/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("%c  Dev by Enrico Bono — https://baivo.studio", "background-color: #222222; color: #f9d63d; font-size:10px; padding:8px 10px 6px; border-radius:4px;"), console.log("hello, World !, lets do this!")
---
+++ out/artifacts/testenv_sakila_Web_exploded/WEB-INF/classes/templates/staticviews.md
@@ -0,0 +1,6 @@
+lets go with views aka templates whow
+
+Static ones and Dynmaic ones
+<pre>
+html lang="en" xmlns:th="https://www.thymeleaf.org/"
+</pre>
